b282eec7a16860a1215c0168bd649eb6467afa8b
Eloy Gonzalez <eloy522752@gmail.com>
2021-04-08 22:29:05 -0400

    add files to repo

---
+++ index.js
@@ -0,0 +1,18 @@
+const generateMarkdownfunc = require('./utils/generateMarkdown')
+//const inquirer = require('inquirer');
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+  
+   console.log(generateMarkdownfunc("Test"));
+}
+
+// Function call to initialize app
+init();
